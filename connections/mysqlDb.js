a4dd092259d6f3ddcbbc891876067a2b458dc32b
gauravlongia <153883949+gauravlongia@users.noreply.github.com>
2024-01-05 17:40:00 +0530

    Update mysqlDb.js

--- connections/mysqlDb.js
+++ connections/mysqlDb.js
@@ -1,13 +1,20 @@
 // db.js
 
 const mysql = require('mysql');
-
+ 
 const db = mysql.createConnection({
-    host: 'coderzbar.info', 
-    user: 'u603640889_nodefutur',
-    password: 'Coderzbar@123',
-    database: 'u603640889_nodefutur'
-}); 
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+// const db = mysql.createConnection({
+//     host: 'coderzbar.info', 
+//     user: 'u603640889_nodefutur',
+//     password: 'Coderzbar@123',
+//     database: 'u603640889_nodefutur'
+// }); 
 db.connect((err) => {
     if (err) throw err;
     console.log('MySQL Connected...');
