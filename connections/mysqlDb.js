9457410e36fb56079d7cac120b359103ce6cdf04
gauravlongia <153883949+gauravlongia@users.noreply.github.com>
2024-01-05 17:04:37 +0530

    Update mysqlDb.js

--- connections/mysqlDb.js
+++ connections/mysqlDb.js
@@ -2,13 +2,18 @@
 
 const mysql = require('mysql');
 
+// const db = mysql.createConnection({
+//     host: 'https://coderzbar.info/',
+//     user: 'u603640889_nodefutur',
+//     password: 'Coderzbar@123',
+//     database: 'u603640889_nodefutur'
+// });
 const db = mysql.createConnection({
-    host: 'https://coderzbar.info/',
-    user: 'u603640889_nodefutur',
-    password: 'Coderzbar@123',
-    database: 'u603640889_nodefutur'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodeapp'
 });
-
 db.connect((err) => {
     if (err) throw err;
     console.log('MySQL Connected...');
